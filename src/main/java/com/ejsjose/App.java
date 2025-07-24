115f3e68cb8a6af6fb08c61f84753138c76ca0dd
EvandroJosias <evandro.josias@yahoo.com.br>
2025-07-24 20:59:48 -0300

    Create Maven Project

---
+++ src/main/java/com/ejsjose/App.java
@@ -0,0 +1,13 @@
+package com.ejsjose;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
